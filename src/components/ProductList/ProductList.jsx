8554957dbe8f23b49fd59d78e0e284e0db473b64
socloccoc <daihusk57@gmail.com>
2024-06-14 15:21:07 +0700

    a

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,14 +5,14 @@ import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Jeans', price: 5000, description: 'Blue, straight'},
+    {id: '2', title: 'Jacket', price: 12000, description: 'Blue, straight'},
+    {id: '3', title: 'Jeans 2', price: 5000, description: 'Blue, straight'},
+    {id: '4', title: 'Jacket 8', price: 122, description: 'Blue, straight'},
+    {id: '5', title: 'Jeans 3', price: 5000, description: 'Blue, straight'},
+    {id: '6', title: 'Jacket 7', price: 600, description: 'Blue, straight'},
+    {id: '7', title: 'Jeans 4', price: 5500, description: 'Blue, straight'},
+    {id: '8', title: 'Jacket 5', price: 12000, description: 'Blue, straight'},
 ]
 
 const getTotalPrice = (items = []) => {
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://85.119.146.179:8000/web-data', {
+        fetch('http://localhost:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
